b19e898b92a55dbaba9a3983d88255cfe376bcf6
chukwuma oparaeke <coparaeke2020@gmail.com>
2020-12-10 11:08:09 +0100

    private files

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-import express from "express";
-import mongoose from "mongoose";
+ const express = require("express");
+ const mongoose = require("mongoose")
+ const bodyParser = require("body-parser")
+ const cookieParser = require("cookie-parser");
+ const {User} = require("./models/user")
+ const config = require("./config/key");
+
 
 const app = express();
 
-mongoose.connect("mongodb+srv://chukmas:sundayoliseh2015@cluster0.d3etk.mongodb.net/travel?retryWrites=true&w=majority",
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser())
+
+mongoose.connect(config.mongoURI,
 { useNewUrlParser: true, useUnifiedTopology: true } )
 .then(()=> console.log ('Connected to Database'))
 .catch(()=>console.log(error))
 
 app.get("/", (req,res)=>{
     res.send("Hello Team");
+})
+
+app.post('api/users/register', (req, res)=>{
+    const user = new User(req.body)
+    user.save((err, userData)=>{
+        if(err) return res.json({success:false, err})
+    })
+    res.status(200).json({success:true, userData: doc})
 })
 
 const port = process.env.PORT || 5000;
